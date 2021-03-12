9ad8d6c801eb096024bf09df3eaf5f230cecabbd
Nicolas Zeferino <nicolas.zeferinosi@gmail.com>
2021-03-12 13:36:07 -0300

    Switch map / workflowOperators

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -4,6 +4,7 @@ import { FormControl } from '@angular/forms';
 import { Acoes } from './modelo/acoes';
 import { AcoesService } from './acoes.service';
 import { Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-acoes',
@@ -12,7 +13,12 @@ import { Subscription } from 'rxjs';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesService.getAcoes();
+  acoes$ = this.acoesInput.valueChanges
+    .pipe(
+      tap(console.log),switchMap((valorDigitado)=>
+        this.acoesService.getAcoes(valorDigitado)
+      ),tap(console.log)
+    );
   
   constructor(private acoesService:AcoesService) {}
   
--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck, tap } from 'rxjs/operators';
 
@@ -9,8 +9,12 @@ export class AcoesService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAcoes(){
-    return this.httpClient.get<AcoesAPI>("http://localhost:3000/acoes")
+  getAcoes(valor?: string){
+    const params = valor ? new HttpParams()
+      .append('valor',valor) : undefined
+
+    return this.httpClient
+      .get<AcoesAPI>("http://localhost:3000/acoes", {params})
       .pipe(
         tap((valor)=> console.log({valor})),
         pluck('payload'),
