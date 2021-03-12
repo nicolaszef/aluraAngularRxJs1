82e4779f33547342ef84164ec648d4628916cde6
Nicolas Zeferino <nicolas.zeferinosi@gmail.com>
2021-03-12 13:11:41 -0300

    unsubscribe added

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck, tap } from 'rxjs/operators';
 
-import { Acao } from './modelo/acoes';
+import { Acao, AcoesAPI } from './modelo/acoes';
 
 @Injectable({providedIn: 'root'})
 export class AcoesService {
@@ -10,7 +10,7 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) { }
 
   getAcoes(){
-    return this.httpClient.get<any>("http://localhost:3000/acoes")
+    return this.httpClient.get<AcoesAPI>("http://localhost:3000/acoes")
       .pipe(
         tap((valor)=> console.log({valor})),
         pluck('payload'),
